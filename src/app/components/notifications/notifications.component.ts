445328e8bb2b1b11782cab2456f908b7f7008f3c
Javier <javimacarromedina@gmail.com>
2019-08-03 09:17:11 +0200

    Commit nº6 - Pop Up Fin de juego añadido

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+// import { UnePalabrasPage } from './../../pages/une-palabras/une-palabras.page';
+import { PopoverController } from '@ionic/angular';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  // @ViewChild(UnePalabrasPage) hijo: UnePalabrasPage; En caso de que deseasemos trabajar con alguna variable
+
+  constructor(public popoverCtrl: PopoverController) { } // DS006: Implementación de ion-popover para mostrar el final del juego
 
   ngOnInit() {
+    // this.popoverCtrl = this.hijo.popoverCtrl;
   }
 
+  /**
+   * SEJMM DS006: Implementación de ion-popover para mostrar el final del juego
+   */
+  async comeBack () {
+    return await this.popoverCtrl.dismiss();
+  }
 }
