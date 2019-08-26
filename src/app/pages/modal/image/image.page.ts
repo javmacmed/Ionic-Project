d838bff50c977d10269f77c1be613227d24ee202
Javier <javimacarromedina@gmail.com>
2019-08-26 13:25:07 +0200

    Salvar el pc

--- src/app/pages/modal/image/image.page.ts
+++ src/app/pages/modal/image/image.page.ts
@@ -1,6 +1,6 @@
 /* SEJMM DS009; Creación y gestión de formulario */
 import { Component, Input, OnInit } from '@angular/core';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, NumericValueAccessor } from '@ionic/angular';
 // import { DomSanitizer } from '@angular/platform-browser';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { DatabaseService } from 'src/app/services/database.service';
@@ -14,6 +14,8 @@ export class ImagePage implements OnInit {
   @Input() value: any; // Util para pasar un parametro. P.E: Nombre de tabla ya creada
   // public image: any;
   validation_form: FormGroup; // Declaración del grupo de control de formulario
+  elems_form: FormGroup;
+  duplaCount = 0; // Contador del numero de duplas Español/Inglés añadidas al formulario
   validation_messages = { // Mensajes de validación para cada una de las validaciones realizadas en cada control de formulario
     'tableName': [
       { type: 'required', message: 'Nombre de tabla requerido.' },
@@ -22,6 +24,13 @@ export class ImagePage implements OnInit {
       { type: 'pattern', message: 'Nombre de tabla debe contener solo letras y espacios' },
       { type: 'validUsername', message: 'Your username has already been taken.' }
     ],
+    'duplaControlMessages': [
+      { type: 'required', message: 'Nombre de tabla requerido.' },
+      { type: 'minlength', message: 'Nombre de tabla debe tener al menos 2 carácteres.' },
+      { type: 'maxlength', message: 'Nombre de tabla no puede tener más de 15 carácteres.' },
+      { type: 'pattern', message: 'Nombre de tabla debe contener solo letras y espacios' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ]
   };
 
   constructor(
@@ -30,7 +39,7 @@ export class ImagePage implements OnInit {
     // private sanitizer: DomSanitizer,
     public formBuilder: FormBuilder,
     private db: DatabaseService
-  ) {}
+  ) { }
 
   ngOnInit() {
     // this.image = this.sanitizer.bypassSecurityTrustStyle(this.value);
@@ -54,16 +63,37 @@ export class ImagePage implements OnInit {
       // matching_passwords: this.matching_passwords_group,
       // terms: new FormControl(true, Validators.pattern('true'))
     });
+    this.elems_form = this.formBuilder.group({
+
+    });
   }
   closeModal() {
-    this.modalCtrl.dismiss();
+    this.modalCtrl.dismiss('¡Tabla creada!');
   }
-/**
- * @description Añade elementos en "Español" e "Inglés" introducidos en los inputs para dicho proposito.
- * @param values Valores introducidos en el formulario en formato json. PE: {username: "JmacArrow", name: "Javier", lastname: "M" ...
- */
-  addElement(values) {
-    
+  /**
+   * @description Incrementa el número de elementos de control y añade su dupla en español y en inglés.
+   */
+  addControl() {
+    this.elems_form.addControl('spanishName' + (this.duplaCount + 1), new FormControl('', Validators.compose([Validators.maxLength(15), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])));
+    this.elems_form.addControl('englishName' + (this.duplaCount + 1), new FormControl('', Validators.compose([Validators.maxLength(15), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])));
+    console.log('Contador duplas: ' + this.duplaCount);
+    console.log('spanishName' + (this.duplaCount + 1));
+    console.log('englishName' + (this.duplaCount + 1));
+    this.duplaCount++;
+    console.log('Contador duplas después: ' + this.duplaCount);
+
+  }
+  /**
+   * @description Elimina dupla de elementos de control español y en inglés.
+   */
+  removeControl(control) {
+    const controlKey: string = control.key;
+    const controlSuffixNumber: number = parseInt(controlKey.match(/\d+/)[0], 10); // Parseamos identificador de control de entrada y obtenemos su parte numerica
+    const controlLeftColumnKey: string = 'spanishName' + (controlSuffixNumber);
+    console.log('Eliminar control derecho: ' + controlKey);
+    console.log('Eliminar control izquierdo: ' + controlLeftColumnKey);
+    this.elems_form.removeControl(control.key);
+    this.elems_form.removeControl(controlLeftColumnKey);
   }
   /**
    * @description Función encargada de implementar las llamadas adecuadas a la base de datos en función de los campos del formulario
